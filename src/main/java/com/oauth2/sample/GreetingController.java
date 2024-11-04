d94ea2652255bee9c600e3397466e0b2fc3be170
naresshh <mnareshreddy444@gmail.com>
2024-11-03 21:35:51 -0500

    Initial Commit

---
+++ src/main/java/com/oauth2/sample/GreetingController.java
@@ -0,0 +1,13 @@
+package com.oauth2.sample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
